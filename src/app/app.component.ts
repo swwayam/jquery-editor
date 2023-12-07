4c2dee263521607e1ec3557fe14875c1d0d43649
Sunil Kumar <sunkumar@ekcs.co>
2023-12-07 17:11:29 +0530

    add image crop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,19 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { log } from 'console';
 import { FormsModule } from '@angular/forms';
+
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import {
+  NgxPhotoEditorModule,
+  NgxCroppedEvent,
+  NgxPhotoEditorService,
+} from 'ngx-photo-editor';
 declare var $: any;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule],
+  imports: [CommonModule, RouterOutlet, FormsModule, NgxPhotoEditorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -25,12 +31,12 @@ export class AppComponent implements OnInit, OnChanges {
   size = '/assets/test291123a3_assets/test291123a3/test291123a3/index.html';
   txtFields: any[] = [];
   sanitizedURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.size);
+  output?: NgxCroppedEvent;
 
   // allSizes -> reflect changes
-  // local 
+  // local
 
-  allSizes : boolean = false
-
+  allSizes: boolean = false;
 
   iframe: any;
 
@@ -44,8 +50,21 @@ export class AppComponent implements OnInit, OnChanges {
     console.log('changed');
   }
 
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(
+    private sanitizer: DomSanitizer,
+    private service: NgxPhotoEditorService
+  ) {}
 
+  fileChangeHandler($event: any) {
+    this.service
+      .open($event, {
+        aspectRatio: 4 / 3,
+        autoCropArea: 1,
+      })
+      .subscribe((data) => {
+        this.output = data;
+      });
+  }
   // Using filenames we will auto take the sizes
   // type
 
@@ -56,14 +75,13 @@ export class AppComponent implements OnInit, OnChanges {
 
   // Sanitize the URL
 
-  setG(){
-    for(const el of this.data){
-      if(el.size == "all"){
-        el.global = true
+  setG() {
+    for (const el of this.data) {
+      if (el.size == 'all') {
+        el.global = true;
       }
     }
     console.log(this.data);
-    
   }
 
   async onIframeLoad(myFrame: HTMLIFrameElement) {
@@ -79,8 +97,6 @@ export class AppComponent implements OnInit, OnChanges {
     function getActualFontInInt(fontSize: any) {
       return parseInt(fontSize.substring(0, fontSize.length - 2));
     }
-
-    
 
     allID?.forEach((el: any) => {
       let value;
@@ -99,15 +115,15 @@ export class AppComponent implements OnInit, OnChanges {
 
       // console.log(value);
       if (editable[1] == 'txt') {
-          for (const el of this.data) {
-            if(el.size == this.type && el.label == editable[2]){
-              this.changeTxt(el)
-              this.changeColor(el)
-              this.changeFontSize(el)
-              return
-            }
-            console.log(el);
+        for (const el of this.data) {
+          if (el.size == this.type && el.label == editable[2]) {
+            this.changeTxt(el);
+            this.changeColor(el);
+            this.changeFontSize(el);
+            return;
           }
+          console.log(el);
+        }
         this.data.push({
           size: this.type,
           label: editable[2],
@@ -117,6 +133,23 @@ export class AppComponent implements OnInit, OnChanges {
           fontSize: getActualFontInInt(window.getComputedStyle(el).fontSize),
         });
       }
+
+      // console.log(value);
+      // if (editable[1] == 'img') {
+      //   for (const el of this.data) {
+      //     if (el.size == this.type && el.label == editable[2]) {
+      //       this.cropImage(el);
+      //       return;
+      //     }
+      //     console.log(el);
+      //   }
+      //   this.data.push({
+      //     size: this.type,
+      //     label: editable[2],
+      //     pic: value,
+      //     type: 'img',
+      //   });
+      // }
     });
 
     // All -> update
@@ -126,15 +159,38 @@ export class AppComponent implements OnInit, OnChanges {
     // console.log(this.txtFields);
   }
 
-
+  cropImage(src: any) {
+    let id = 'sd_img_Picture';
+    this.service
+      .open(src, {
+        aspectRatio: 4 / 3,
+        autoCropArea: 1,
+      })
+      .subscribe((data) => {
+        this.output = data;
+        this.iframe.querySelector(`#${id}`).src = this.output.base64;
+      });
+  }
+  changeImage($event: any) {
+    let id = 'sd_img_Picture';
+    this.service
+      .open($event, {
+        aspectRatio: 4 / 3,
+        autoCropArea: 1,
+      })
+      .subscribe((data) => {
+        this.output = data;
+        this.iframe.querySelector(`#${id}`).src = this.output.base64;
+      });
+  }
 
   changeTxt(txt: any) {
-    if(txt.global == true){
+    if (txt.global == true) {
       for (const el of this.data) {
-        if(txt.label = el.label){
-          el.color = txt.color
-          el.value = txt.value
-          el.fontSize = txt.fontSize
+        if ((txt.label = el.label)) {
+          el.color = txt.color;
+          el.value = txt.value;
+          el.fontSize = txt.fontSize;
         }
       }
     }
